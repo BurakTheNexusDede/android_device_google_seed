018f2a214013e4f4c034e04d766e350ee128275d
Burak "TheNexus" Dede <burak.dede.03@gmail.com>
2017-06-20 21:45:16 +0300

    seed: AEX Bringup

--- aosp_seed.mk
+++ aosp_seed.mk
@@ -15,13 +15,13 @@
 
 $(call inherit-product, device/google/seed/full_seed.mk)
 
-# Inherit some common Unicorn stuff.
-$(call inherit-product, vendor/du/config/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8916
 
-PRODUCT_NAME := du_seed
+PRODUCT_NAME := aosp_seed
 PRODUCT_DEVICE := seed
 PRODUCT_BRAND := google
 PRODUCT_MODEL := seed
@@ -33,3 +33,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=seed \
     BUILD_FINGERPRINT=google/seed_l8150/seed:7.1.1/N4F26W/3815918:user/release-keys \
     PRIVATE_BUILD_DESC="seed_l8150-user 7.1.1 N4F26W 3815918 release-keys"
+
+# Build Magisk
+DEFAULT_ROOT_METHOD := magisk
